1763e7595158ad9c175b11f3b4b1f9a563007d30
Hyunwoo Shim <hkhunk0202@icloud.com>
2023-06-09 20:50:51 +0900

    cerate board function

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,64 @@
 use bevy::prelude::*;
 
-fn setup(mut commands: Commands,
-    mut meshes: ResMut<Assets<Mesh>>,
-    mut materials: ResMut<Assets<StandardMaterial>>
-) {
-    commands
-        .spawn(PbrBundle {
-                mesh: meshes.add(Mesh::from(shape::Plane{ size: 8.0, subdivisions: 0 })),
-                material: materials.add(Color::rgb(1.,0.9,0.9).into()),
-                transform: Transform::from_translation(Vec3::new(4., 0., 4.)),
-                ..Default::default()
-    });
-    commands
-        .spawn(Camera3dBundle {
-            transform: Transform::from_matrix(Mat4::from_rotation_translation(
-                Quat::from_xyzw(-0.3, -0.5, -0.3, 0.5).normalize(),
-                Vec3::new(-0.7, 20.0, 4.0),
-            )),
-            ..Default::default()
-    });
-    commands
-        .spawn(PointLightBundle {
-            transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
-            ..Default::default()
-    });
-}
-
 fn main() {
     App::new()
         .insert_resource(Msaa::default())
         .add_plugins(DefaultPlugins.set(WindowPlugin {
             primary_window: Some(Window {
-                resolution: (1200., 1200.).into(),
+                resolution: (1600., 1600.).into(),
                 title: "Chess".to_string(),
                 ..Default::default()
             }),
             ..Default::default()
         }))
         .add_startup_system(setup)
+        .add_startup_system(create_board)
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_matrix(Mat4::from_rotation_translation(
+            Quat::from_xyzw(-0.4, -0.5, -0.4, 0.5).normalize(),
+            //Quat::from_xyzw(-0.3,-0.5,-0.3,0.5).normalize(),
+            Vec3::new(-0.7, 20.0, 4.0),
+        )),
+        ..Default::default()
+    });
+    commands.spawn(PointLightBundle {
+        transform: Transform::from_translation(Vec3::new(4.0, 8.0, 4.0)),
+        ..Default::default()
+    });
+}
+
+fn create_board(
+    mut commands: Commands,
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    let mesh = meshes.add(Mesh::from(shape::Plane {
+        size: 1.0,
+        subdivisions: 0,
+    }));
+    let white_material = materials.add(Color::rgb(1.0, 0.9, 0.9).into());
+    let black_material = materials.add(Color::rgb(0.0, 0.1, 0.1).into());
+
+    for i in 0..8 {
+        for j in 0..8 {
+            commands.spawn(PbrBundle {
+                mesh: mesh.clone(),
+                material: if (i + j + 1) % 2 == 0 {
+                    white_material.clone()
+                } else {
+                    black_material.clone()
+                },
+                transform: Transform::from_translation(Vec3::new(i as f32, 0.0, j as f32)),
+                ..Default::default()
+            });
+        }
+    }
 }
